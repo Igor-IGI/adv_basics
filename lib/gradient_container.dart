b622da278fda1dae3feb441e99f89276fca6b005
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-08 23:05:54 +0200

    add color and alignment

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+  static const startAlignment = Alignment.topLeft;
+  static const endAlignment = Alignment.bottomRight;
 
   @override
   Widget build(context) {
-    throw UnimplementedError();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+    );
   }
 }
