d4e036ec1693cdbab4de282fb15aeeec0272d97d
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-08 23:03:07 +0200

    add custom widget and colors for background

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:adv_basics/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: GradientContainer(
+          colors: [
+            Color.fromRGBO(81, 45, 168, 1),
+            Color.fromRGBO(126, 87, 194, 1),
+            Color.fromRGBO(179, 157, 219, 1),
+          ],
+        ),
+      ),
     ),
   );
 }
