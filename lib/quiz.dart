41dbc499eae97b116e9e7b84557de638a0db3f48
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-10 22:49:21 +0200

    add swichScreen function

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/question_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
 
@@ -11,6 +12,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _Quiz extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
