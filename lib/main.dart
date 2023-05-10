4365a231ceda011b476388594fefc99b97a57bba
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-10 21:57:02 +0200

    add container for colors and alignment

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(
-          colors: [
-            Color.fromRGBO(81, 45, 168, 1),
-            Color.fromRGBO(126, 87, 194, 1),
-            Color.fromRGBO(179, 157, 219, 1),
-          ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(81, 45, 168, 1),
+                Color.fromRGBO(126, 87, 194, 1),
+                Color.fromRGBO(179, 157, 219, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
         ),
       ),
     ),
