a898c81e5ad8010e8d19f113d603c166c549d5e1
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-10 22:40:08 +0200

    add new start widget Quiz to runApp function

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(81, 45, 168, 1),
-                Color.fromRGBO(126, 87, 194, 1),
-                Color.fromRGBO(179, 157, 219, 1),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(Quiz());
 }
