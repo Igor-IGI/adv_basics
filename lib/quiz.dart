f102f35a4d91f8dd45a9932dae523265485ea26a
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-10 22:39:42 +0200

    add new stateful widget Quiz

---
+++ lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _Quiz();
+  }
+}
+
+class _Quiz extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(81, 45, 168, 1),
+                Color.fromRGBO(126, 87, 194, 1),
+                Color.fromRGBO(179, 157, 219, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
